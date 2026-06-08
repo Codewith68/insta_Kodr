325b9e08bf43c1cf4949e6a6d19c41606e2f8973
Subrat Palei <subratpaleibabul@gmail.com>
2026-06-08 17:56:25 +0530

    Initialize Socket.IO on the HTTP server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import config from "./src/config/config.js";
 import { connectDB } from "./src/config/db.config.js";
 import { initSocket } from "./src/socket.js";
 const server = http.createServer(app);
-
 // Initialize Socket.IO on the HTTP server
 initSocket(server);
 server.listen(config.PORT, () => {
