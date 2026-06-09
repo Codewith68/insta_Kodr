b4d8cdaa8147ebb52440ca49d12eac43cac59608
Subrat Palei <subratpaleibabul@gmail.com>
2026-06-09 18:49:10 +0530

    Fix comment formatting for Socket.IO initialization

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import config from "./src/config/config.js";
 import { connectDB } from "./src/config/db.config.js";
 import { initSocket } from "./src/socket.js";
 const server = http.createServer(app);
-// Initialize Socket.IO on the HTTP server
+//Initialize Socket.IO on the HTTP server
 initSocket(server);
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
